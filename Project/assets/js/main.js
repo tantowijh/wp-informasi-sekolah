bb5ea641aaecec2b7bb85a83253006d9febc8bd7
Tantowi JH <71215938+tantowijh@users.noreply.github.com>
2023-12-06 09:15:35 +0700

    update

--- Project/assets/js/main.js
+++ Project/assets/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var navbar = document.querySelector('.zul-nav-scroll');
     var prevScrollpos = window.scrollY;
 
@@ -11,4 +11,12 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         prevScrollpos = currentScrollPos;
     });
+
+
+    var currentPageUrl = window.location.href;
+    var footer = document.querySelector('.zuledu-footer-container');
+    // Apakah url saat ini mengandung "search.php"
+    if (currentPageUrl.indexOf('search.php') !== -1) {
+        footer.classList.add('fixed-bottom');
+    }
 });
--- Project/search.php
+++ Project/search.php
@@ -1,42 +1,42 @@
 <?php
-    require('header.php');
-    include('breadcrumb.php');
+require('header.php');
+include('breadcrumb.php');
 ?>
 
-<div class="container">
+<div class="container pb-4">
 
-<?php
-if (isset($_POST['search'])) {
-    $searchTerm = $_POST['search'];
+    <?php
+    if (isset($_POST['search'])) {
+        $searchTerm = $_POST['search'];
 
-    // Replace this array with your actual data
-    $data = array(
-        "Item 1",
-        "Item 2",
-        "Item 3",
-        // Add more items as needed
-    );
+        // Replace this array with your actual data
+        $data = array(
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            // Add more items as needed
+        );
 
-    $results = array();
+        $results = array();
 
-    foreach ($data as $item) {
-        if (stripos($item, $searchTerm) !== false) {
-            $results[] = $item;
+        foreach ($data as $item) {
+            if (stripos($item, $searchTerm) !== false) {
+                $results[] = $item;
+            }
+        }
+
+        if (!empty($results)) {
+            echo "<h2>Search Results</h2>";
+            echo "<ul>";
+            foreach ($results as $result) {
+                echo "<li>{$result}</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo "No results found.";
         }
     }
-
-    if (!empty($results)) {
-        echo "<h2>Search Results</h2>";
-        echo "<ul>";
-        foreach ($results as $result) {
-            echo "<li>{$result}</li>";
-        }
-        echo "</ul>";
-    } else {
-        echo "No results found.";
-    }
-}
-?>
+    ?>
 
 </div>
 
